88a408c428aa413bc9eee404715bc62124776ac3
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-06 12:07:35 -0700

    add stub for the circuit

--- scripts/prove.sh
+++ scripts/prove.sh
@@ -6,29 +6,29 @@ set -e
 
 # Compile the circuit
 echo "****COMPILING CIRCUIT****"
-circom circuits/MerkleTreeChecker.circom --r1cs --wasm --sym --c -o build
+circom circuits/ERC721OwnershipChecker.circom --r1cs --wasm --sym --c -o build
 
 # Generate witness
 echo "****GENERATING WITNESS FOR SAMPLE INPUT****"
-node build/MerkleTreeChecker_js/generate_witness.js build/MerkleTreeChecker_js/MerkleTreeChecker.wasm input.json build/witness.wtns
+node build/ERC721OwnershipChecker_js/generate_witness.js build/ERC721OwnershipChecker_js/ERC721OwnershipChecker.wasm input.json build/witness.wtns
 
 # Setup (use plonk so we can skip ptau phase 2, generate zkey 0000
 echo "****GENERATING ZKEY 0000****"
-yarn snarkjs groth16 setup build/MerkleTreeChecker.r1cs pot/pot24_final.ptau pot/OwnershipChecker_0000.zkey
+yarn snarkjs groth16 setup build/ERC721OwnershipChecker.r1cs pot/pot24_final.ptau pot/OwnershipChecker_0000.zkey
 
 # Generate reference zkey
-# yarn snarkjs zkey new build/MerkleTreeChecker.r1cs pot/pot24_final.ptau pot/MerkleTreeChecker_0000.zkey
+# yarn snarkjs zkey new build/ERC721OwnershipChecker.r1cs pot/pot24_final.ptau pot/ERC721OwnershipChecker_0000.zkey
 
 # Ceremony just like before but for zkey this time
-# yarn snarkjs zkey contribute pot/MerkleTreeChecker_0000.zkey pot/MerkleTreeChecker_0001.zkey \
+# yarn snarkjs zkey contribute pot/ERC721OwnershipChecker_0000.zkey pot/ERC721OwnershipChecker_0001.zkey \
 #   --name="First contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
-# yarn snarkjs zkey contribute pot/MerkleTreeChecker_0001.zkey pot/MerkleTreeChecker_0002.zkey \
+# yarn snarkjs zkey contribute pot/ERC721OwnershipChecker_0001.zkey pot/ERC721OwnershipChecker_0002.zkey \
 #   --name="Second contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
-# yarn snarkjs zkey contribute pot/MerkleTreeChecker_0002.zkey pot/MerkleTreeChecker_0003.zkey \
+# yarn snarkjs zkey contribute pot/ERC721OwnershipChecker_0002.zkey pot/ERC721OwnershipChecker_0003.zkey \
 #   --name="Third contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
 
 #  Verify zkey
-# yarn snarkjs zkey verify build/MerkleTreeChecker.r1cs pot/pot24_final.ptau pot/MerkleTreeChecker_0001.zkey
+# yarn snarkjs zkey verify build/ERC721OwnershipChecker.r1cs pot/pot24_final.ptau pot/ERC721OwnershipChecker_0001.zkey
 
 # Apply random beacon as before
 echo "****GENERATING FINAL ZKEY****"
@@ -37,7 +37,7 @@ yarn snarkjs zkey beacon pot/OwnershipChecker_0000.zkey pot/OwnershipChecker_fin
 
 # Optional: verify final zkey
 echo "****VERIFYING FINAL ZKEY****"
-yarn snarkjs zkey verify build/MerkleTreeChecker.r1cs pot/pot24_final.ptau pot/OwnershipChecker_final.zkey
+yarn snarkjs zkey verify build/ERC721OwnershipChecker.r1cs pot/pot24_final.ptau pot/OwnershipChecker_final.zkey
 
 # Export verification key
 echo "** Exporting vkey"
@@ -54,7 +54,7 @@ yarn snarkjs groth16 verify pot/verification_key.json build/public.json build/pr
 
 # Smart contract commands
 # Export the verifier as a smart contract
-yarn snarkjs zkey export solidityverifier pot/MerkleTreeChecker_final.zkey contracts/Verifier.sol
+yarn snarkjs zkey export solidityverifier pot/ERC721OwnershipChecker_final.zkey contracts/Verifier.sol
 
 # Create the solidity call data with the existing public.json and proof.json
 # snarkjs zkey export soliditycalldata public.json proof.json
