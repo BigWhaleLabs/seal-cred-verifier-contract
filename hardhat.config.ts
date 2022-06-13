d05ee171230aece0bb7b7d7e4af46f2ffe3087a0
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-13 15:35:37 -0700

    fix actions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv'
-import { cleanEnv, str } from 'envalid'
+import { cleanEnv, str, testOnly } from 'envalid'
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
@@ -10,14 +10,21 @@ import { ETH_RPC as FALLBACK_ETH_RPC } from '@big-whale-labs/constants'
 
 dotenv.config()
 
-const { CONTRACT_OWNER_PRIVATE_KEY, ETH_RPC, ETHERSCAN_API_KEY } = cleanEnv(
-  process.env,
-  {
-    CONTRACT_OWNER_PRIVATE_KEY: str(),
-    ETH_RPC: str({ default: FALLBACK_ETH_RPC }),
-    ETHERSCAN_API_KEY: str(),
-  }
-)
+const {
+  CONTRACT_OWNER_PRIVATE_KEY,
+  ETH_RPC,
+  ETHERSCAN_API_KEY,
+  COINMARKETCAP_API_KEY,
+} = cleanEnv(process.env, {
+  CONTRACT_OWNER_PRIVATE_KEY: str({
+    devDefault: testOnly(
+      '0000000000000000000000000000000000000000000000000000000000000000'
+    ),
+  }),
+  ETH_RPC: str({ default: FALLBACK_ETH_RPC }),
+  ETHERSCAN_API_KEY: str({ devDefault: testOnly('') }),
+  COINMARKETCAP_API_KEY: str({ devDefault: testOnly('') }),
+})
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -41,7 +48,8 @@ const config: HardhatUserConfig = {
   },
   gasReporter: {
     enabled: true,
-    currency: 'ETH',
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
