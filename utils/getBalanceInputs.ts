155f935098aa1df968d8b400026c3c4649cb406e
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-06 16:25:26 -0700

    extract inputs for message

--- utils/getBalanceInputs.ts
+++ utils/getBalanceInputs.ts
@@ -3,22 +3,18 @@ import { utils } from 'ethers'
 import eddsaSign from './eddsaSign'
 import getSignatureInputs from './getSignatureInputs'
 
-export default async function (
-  balance = '0x6b87c4e204970e6',
-  threshold = '0x1',
-  ownerAddress = '0xbf74483DB914192bb0a9577f3d8Fb29a6d4c08eE',
-  tokenAddress = '0x722B0676F457aFe13e479eB2a8A4De88BA15B2c6'
+async function inputsForMessage(
+  message: string,
+  balance?: string,
+  threshold?: string
 ) {
-  // Message
-  const message = `${ownerAddress}owns${tokenAddress}g`
-  const messageUInt8 = utils.toUtf8Bytes(message)
-  const mimc7 = await buildMimc7()
-  const M = mimc7.multiHash([...messageUInt8, balance])
-  // EdDSA
-  const { publicKey, signature } = await eddsaSign(M)
-  // Generating inputs
   const babyJub = await buildBabyjub()
   const F = babyJub.F
+  const messageUInt8 = utils.toUtf8Bytes(message)
+  const messageBytes = balance ? [...messageUInt8, balance] : messageUInt8
+  const mimc7 = await buildMimc7()
+  const M = mimc7.multiHash(messageBytes)
+  const { publicKey, signature } = await eddsaSign(M)
   return {
     message: Array.from(messageUInt8),
     balance,
@@ -29,6 +25,18 @@ export default async function (
     R8y: F.toObject(signature.R8[1]).toString(),
     S: signature.S.toString(),
     M: F.toObject(M).toString(),
+  }
+}
+
+export default async function (
+  balance = '0x6b87c4e204970e6',
+  threshold = '0x1',
+  ownerAddress = '0xbf74483DB914192bb0a9577f3d8Fb29a6d4c08eE',
+  tokenAddress = '0x722B0676F457aFe13e479eB2a8A4De88BA15B2c6'
+) {
+  const message = `${ownerAddress}owns${tokenAddress}g`
+  return {
+    ...(await inputsForMessage(message, balance, threshold)),
     ...(await getSignatureInputs()),
   }
 }
