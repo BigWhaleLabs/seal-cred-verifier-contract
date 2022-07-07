2838f547fb98bf0ed6fc266a20257af38dd16d9a
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-06 17:19:01 -0700

    fix the balance input generation bug

--- utils/getBalanceInputs.ts
+++ utils/getBalanceInputs.ts
@@ -42,7 +42,7 @@ export default async function (
       balance,
       threshold
     )),
-    ...(await inputsForMessage(ownerAddress, 'Address', balance, threshold)),
+    ...(await inputsForMessage(ownerAddress, 'Address')),
     ...(await getSignatureInputs()),
   }
 }
