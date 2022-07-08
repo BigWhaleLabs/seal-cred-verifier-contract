476819d5551859ebab8d00895aa97521d8c3276f
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-08 14:15:23 -0700

    fix balance number

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,7 +8,10 @@ async function main() {
   const [deployer] = await ethers.getSigners()
   // Deploy the contract
   console.log('Deploying contracts with the account:', deployer.address)
-  console.log('Account balance:', (await deployer.getBalance()).toString())
+  console.log(
+    'Account balance:',
+    utils.formatEther(await deployer.getBalance())
+  )
   const provider = ethers.provider
   const { chainId } = await provider.getNetwork()
   const chains = {
